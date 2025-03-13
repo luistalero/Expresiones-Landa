1b83c71e2c09e17bdba7a710a00122c83a49b6f9
luistalero <luisalbertotaleromartinez@gmail.com>
2025-03-13 14:37:12 -0500

    Feat: :tada: Commit 1: Inicio de la clase Primer prueba de expresiones lambda

---
+++ src/main/java/com/expresionlanda/Main.java
@@ -0,0 +1,11 @@
+package com.expresionlanda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> lista = Arrays.asList("uno", "dos", "tres");
+        lista.forEach(s -> System.out.println(s));
+    }
+}
